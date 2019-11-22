48e284de938f3d38659f0b38a98cb6efce33a024
Paige T <ptuttosi@sfu.ca>
2019-11-21 19:58:58 -0800

    Update python NN for our dataset

--- NN.py
+++ NN.py
@@ -1,50 +1,47 @@
 import pandas as pd
 import numpy as np
 
-cd = pd.read_csv("cleanData.csv")
-
 def sigmoid(x):
-    return 1.0/(1+ np.exp(-x))
+  return 1.0/(1+ np.exp(-x))
 
 def sigmoid_derivative(x):
-    return x * (1.0 - x)
+  return x * (1.0 - x)
 
 #neural network class
 #use 4 neurons
 class NeuralNetwork:
-    def __init__(self, x, y):
-        self.input      = x
-        self.weights1   = np.random.rand(self.input.shape[1],4) 
-        self.weights2   = np.random.rand(4,1)                  
-        self.y          = y
-        self.output     = np.zeros(y.shape)
+  def __init__(self, x, y):
+    self.input      = x
+    self.weights1   = np.random.rand(self.input.shape[1],4) 
+    self.weights2   = np.random.rand(4,1)                  
+    self.y          = y
+    self.output     = np.zeros(y.shape)
 
-    #feed forward
-    #bias is 0 or we would have + bias after dot product
-    def feedforward(self):
-            self.layer1 = sigmoid(np.dot(self.input, self.weights1))
-            self.output = sigmoid(np.dot(self.layer1, self.weights2))
+  #feed forward
+  #bias is 0 or we would have + bias after dot product
+  def feedforward(self):
+    self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+    self.output = sigmoid(np.dot(self.layer1, self.weights2))
 
 
-    def backprop(self):
-           # chain rule for derivative of the loss function (SSE) wrt weights2 and weights1
-           d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
-           d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
-           # update the weights with the derivative (slope) of the loss function
-           self.weights1 += d_weights1
-           self.weights2 += d_weights2
+  def backprop(self):
+    # chain rule for derivative of the loss function (SSE) wrt weights2 and weights1
+    d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+    d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
+    # update the weights with the derivative (slope) of the loss function
+    self.weights1 += d_weights1
+    self.weights2 += d_weights2
 
-if __name__ == "__main__":
-    X = np.array([[0,0,1],
-                  [0,1,1],
-                  [1,0,1],
-                  [1,1,1]])
-    y = np.array([[0],[1],[1],[0]])
-    nn = NeuralNetwork(X,y)
+cd = pd.read_csv("cleanData.csv")
 
-    for i in range(1000000):
-        nn.feedforward()
-        nn.backprop()
+churn = cd["churn"]
+regressors = cd.loc[:, cd.columns != 'churn']
 
-    print(nn.output)
+nn = NeuralNetwork(regressors,churn)
+
+for i in range(10):
+    nn.feedforward()
+    nn.backprop()
+
+print(nn.output)
 
