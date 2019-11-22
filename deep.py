5dbc7836b34c22e8c10b572bace6e1983371ccc2
Paige T <ptuttosi@sfu.ca>
2019-11-21 19:28:03 -0800

    Test one way anova

--- deep.py
+++ deep.py
@@ -1,9 +1,13 @@
+"""this code was informed from the following repsitory
+https://github.com/shakedzy/dython"""
+
 import pandas as pd
 import scipy.stats as ss
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+#cramersV to find association between categorical variables
 def cramers_v(x, y):
   confusion_matrix = pd.crosstab(x,y)
   chi2 = ss.chi2_contingency(confusion_matrix)[0]
@@ -14,24 +18,6 @@ def cramers_v(x, y):
   rcorr = r-((r-1)**2)/(n-1)
   kcorr = k-((k-1)**2)/(n-1)
   return np.sqrt(phi2corr/min((kcorr-1),(rcorr-1)))
-
-def correlation_ratio(categories, measurements):
-    fcat, _ = pd.factorize(categories)
-    cat_num = np.max(fcat)+1
-    y_avg_array = np.zeros(cat_num)
-    n_array = np.zeros(cat_num)
-    for i in range(0,cat_num):
-        cat_measures = measurements[np.argwhere(fcat == i).flatten()]
-        n_array[i] = len(cat_measures)
-        y_avg_array[i] = np.average(cat_measures)
-    y_total_avg = np.sum(np.multiply(y_avg_array,n_array))/np.sum(n_array)
-    numerator = np.sum(np.multiply(n_array,np.power(np.subtract(y_avg_array,y_total_avg),2)))
-    denominator = np.sum(np.power(np.subtract(measurements,y_total_avg),2))
-    if numerator == 0:
-        eta = 0.0
-    else:
-        eta = np.sqrt(numerator/denominator)
-    return eta
 
 cd = pd.read_csv("cleanData.csv")
 
@@ -49,12 +35,12 @@ for i in range(0,len(columns)):
           corr[columns[i]][columns[j]] = cell
           corr[columns[j]][columns[i]] = cell
         else:
-          cell = round(correlation_ratio(cd[columns[i]], cd[columns[j]]),4)
+          cell = round((ss.f_oneway(cd[columns[i]], cd[columns[j]])[1]),4)
           corr[columns[i]][columns[j]] = cell
           corr[columns[j]][columns[i]] = cell
       else:
         if columns[j] not in numeric_columns:
-          cell = round(correlation_ratio(cd[columns[j]], cd[columns[i]]),4)
+          cell = round((ss.f_oneway(cd[columns[j]], cd[columns[i]])[1]),4)
           corr[columns[i]][columns[j]] = cell
           corr[columns[j]][columns[i]] = cell
         else:
@@ -70,7 +56,7 @@ sns.heatmap(
   yticklabels=corr.columns.values
 )
 
-plt.savefig('corr.png')
+plt.savefig('corr2.png')
 
 
 
