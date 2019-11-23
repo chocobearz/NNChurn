f583275cb1cb43e73fa2e6202524e2e4e6aefb07
Paige T <ptuttosi@sfu.ca>
2019-11-23 15:51:11 -0800

    update array sizes

--- NN.py
+++ NN.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import sys
+import numpy
+numpy.set_printoptions(threshold=10000)
 
 def sigmoid(x):
   return 1.0/(1+ np.exp(-x))
@@ -12,8 +15,8 @@ def sigmoid_derivative(x):
 class NeuralNetwork:
   def __init__(self, x, y):
     self.input      = x
-    self.weights1   = np.random.rand(self.input.shape[1],4) 
-    self.weights2   = np.random.rand(4,1)                  
+    self.weights1   = np.random.rand(self.input.shape[1],7043) 
+    self.weights2   = np.random.rand(7043,1)                  
     self.y          = y
     self.output     = np.zeros(y.shape)
 
@@ -26,22 +29,47 @@ class NeuralNetwork:
 
   def backprop(self):
     # chain rule for derivative of the loss function (SSE) wrt weights2 and weights1
-    d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
-    d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
+    d_weights2 = np.dot(
+      self.layer1.T, (
+        2*(
+          self.y - self.output
+        ) * sigmoid_derivative(
+          self.output
+        )
+      )
+    )
+    d_weights1 = np.dot(
+      self.input.T, (
+        np.dot(
+          2*(
+            self.y - self.output
+            exit(0)
+          ) * sigmoid_derivative(
+            self.output
+          ), self.weights2.T
+        ) * sigmoid_derivative(
+          self.layer1
+        )
+      )
+    )
+    exit(0)
     # update the weights with the derivative (slope) of the loss function
     self.weights1 += d_weights1
     self.weights2 += d_weights2
 
-cd = pd.read_csv("cleanData.csv")
-
+cd = pd.read_csv("oneHotNNData.csv")
 churn = cd["churn"]
 regressors = cd.loc[:, cd.columns != 'churn']
+churn = churn.to_numpy()
+cd = cd.to_numpy()
 
 nn = NeuralNetwork(regressors,churn)
+nn.feedforward()
+nn.backprop()
 
-for i in range(10):
-    nn.feedforward()
-    nn.backprop()
+#for i in range(10):
+    #nn.feedforward()
+    #nn.backprop()
 
-print(nn.output)
+#print(nn.output)
 
