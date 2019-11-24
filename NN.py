0b5af2cb1fa170dcb500f71ea67fe1add664c9da
Paige T <ptuttosi@sfu.ca>
2019-11-23 16:57:11 -0800

    change netowrk paramters

--- NN.py
+++ NN.py
@@ -15,8 +15,8 @@ def sigmoid_derivative(x):
 class NeuralNetwork:
   def __init__(self, x, y):
     self.input      = x
-    self.weights1   = np.random.rand(self.input.shape[1],7043) 
-    self.weights2   = np.random.rand(7043,1)                  
+    self.weights1   = np.random.rand(self.input.shape[1],31) 
+    self.weights2   = np.random.rand(31,1)                  
     self.y          = y
     self.output     = np.zeros(y.shape)
 
